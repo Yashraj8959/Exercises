0c049793e72f4936d97bfed133c1d7ce104b5dab
Yashraj8959 <ykushwah7828@gmail.com>
2025-02-21 12:32:53 +0530

    Enhance socket.io server: add message handling and improve connection logs

--- socket.io_server/server.js
+++ socket.io_server/server.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 3000
 const server = require('http').createServer(app);   // Create an HTTP server and pass the Express app to it
 const io = require('socket.io')(server);
 io.on('connection', socket => {
-    console.log('A user connected');
+    socket.on('message', (data) => {
+        console.log('Received message:', data);
+        io.emit('chat-message', data);  // Broadcast the message to all connected clients
+    })
+    console.log('User connected');  // Log when a user connects to the server
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+        console.log('User disconnected');
+    })
 
 });
 server.listen(port, () => {          // Start the server on the specified port
